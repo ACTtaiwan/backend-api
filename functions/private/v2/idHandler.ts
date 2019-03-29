5c5b1fe0f19ccd6dc7ac76937d2fe7e34bf72b34
Di-Wei Huang <bdwhuang@gmail.com>
2019-03-29 00:23:17 -0700

    Add a GET param `lang` to id, bill, and person endpoints (#40)

--- functions/private/v2/idHandler.ts
+++ functions/private/v2/idHandler.ts
@@ -2,17 +2,21 @@ import * as _ from 'lodash';
 import { DataGraph, Id } from '../../../libs/dbLib2/DataGraph';
 import { translateTypeEnum } from './handlers';
 import { AssocFieldResolver } from './AssocFieldResolver';
+import { LanguageHelper } from './LanguageHelper';
 
 export class IdHandler {
   public static async run (
     ids: Id[],
     fields: string[],
+    lang?: string,
   ): Promise<any> {
     let g = await DataGraph.getDefault();
+    fields = LanguageHelper.augmentFields(fields, lang);
     let ents = await Promise.all(
       _.map(ids, async id => g.loadEntity(id, _.isEmpty(fields) ? undefined : fields))
     );
     ents = await AssocFieldResolver.resolve(g, ents, fields);
+    ents = LanguageHelper.consolidateFields(ents, lang);
 
     return _.map(ents, translateTypeEnum);
   }
