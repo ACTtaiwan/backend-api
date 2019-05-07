81274d5220f6ab18a63c3a52da29530074f8f1e9
Robin Liao <yilia@microsoft.com>
2019-05-07 13:14:04 -0700

    docker-push

---
+++ docker-push-prod.sh
@@ -0,0 +1,9 @@
+HOST="acttaiwan.azurecr.io"
+IMG_TAG="ustw-backend-prod:latest"
+USER="acttaiwan"
+PASS=""
+
+docker build -t $IMG_TAG .
+docker login $HOST -u $USER -p $PASS
+docker tag $IMG_TAG $HOST/$IMG_TAG
+docker push $HOST/$IMG_TAG
