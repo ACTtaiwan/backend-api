fffb10801960a5d75788dc5571a4414cd7e48b40
Robin Liao <yilia@microsoft.com>
2019-05-09 03:23:05 -0700

    move Azure creds to .env

--- docker-push-dev.sh
+++ docker-push-dev.sh
@@ -1,7 +1,11 @@
+source .env
+
 HOST="acttaiwan.azurecr.io"
 IMG_TAG="ustw-backend-dev:latest"
-USER="acttaiwan"
-PASS=""
+USER=$ACR_USER
+PASS=$ACR_PASS
+
+echo "[Azure Container Registry] USER=$USER  PASS=$PASS"
 
 docker build -t $IMG_TAG .
 docker login $HOST -u $USER -p $PASS
